44b10e18914adf7a75ca71c9bf0ff18d16dd1807
Michał Szczepaniak <m.szczepaniak.000@gmail.com>
2023-08-21 20:53:23 +0200

    Formatting fixes

--- src/responses/current_power_result.rs
+++ src/responses/current_power_result.rs
@@ -1,8 +1,6 @@
 use serde::{Deserialize, Serialize};
-use time::OffsetDateTime;
 
 use crate::responses::TapoResponseExt;
-use crate::tapo_date_format;
 
 /// Contains local time, current power and the energy usage and runtime for today and for the current month.
 #[derive(Debug, Serialize, Deserialize)]
