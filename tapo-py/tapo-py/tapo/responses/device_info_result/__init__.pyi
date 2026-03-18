6c9507c7741e00e74a0074a1ee91f6144134eba1
Mihai Dinculescu <mihai.dinculescu@outlook.com>
2026-03-18 22:23:35 +0000

    refactor(tapo-py): drop redundant _result suffix from device info .pyi file names

--- tapo-py/tapo-py/tapo/responses/device_info_result/__init__.pyi
+++ tapo-py/tapo-py/tapo/responses/device_info_result/__init__.pyi
@@ -1,14 +1,14 @@
-from .color_light_result import *
-from .default_state import *
-from .basic_result import *
-from .hub_result import *
-from .light_result import *
-from .plug_energy_monitoring_result import *
-from .plug_result import *
-from .power_status import *
-from .power_strip_result import *
-from .rgb_light_strip_result import *
-from .rgbic_light_strip_result import *
+from .basic import *
+from .color_light import *
 from .default_plug_state import *
+from .default_state import *
+from .hub import *
+from .light import *
+from .plug import *
+from .plug_energy_monitoring import *
+from .power_status import *
+from .power_strip import *
+from .rgb_light_strip import *
+from .rgbic_light_strip import *
 
 from tapo.requests import LightingEffect as LightingEffect
