32fa31cfad604303a15f7438cc06a0a6ae6c8116
Mihai Dinculescu <mihai.dinculescu@outlook.com>
2026-08-10 19:31:08 +0100

    feat(tapo): add a typed get_general_device_list to the camera hub handler

--- tapo/src/requests/tapo_request.rs
+++ tapo/src/requests/tapo_request.rs
@@ -7,11 +7,8 @@ use super::{
     GetEnergyDataParams, GetPowerDataParams, GetTriggerLogsParams, HandshakeParams, LightingEffect,
     LoginDeviceParams, MultipleRequestParams, PlayAlarmParams, RemoveTimersParams,
     SecurePassthroughParams, SegmentEffect, SmartCamControlChildParams, SmartCamDoParams,
-    SmartCamGetChildDeviceListParams, SmartCamGetParams,
+    SmartCamGetChildDeviceListParams, SmartCamGetGeneralDeviceListParams, SmartCamGetParams,
 };
-
-#[cfg(feature = "debug")]
-use super::SmartCamGetGeneralDeviceListParams;
 
 #[derive(Debug, Serialize)]
 #[serde(rename_all = "snake_case")]
@@ -60,7 +57,6 @@ pub(crate) enum TapoRequest {
     #[cfg(feature = "debug")]
     #[serde(rename = "getChildDeviceComponentList")]
     SmartCamGetChildDeviceComponentList(TapoParams<SmartCamGetChildDeviceListParams>),
-    #[cfg(feature = "debug")]
     #[serde(rename = "getGeneralDeviceList")]
     SmartCamGetGeneralDeviceList(TapoParams<SmartCamGetGeneralDeviceListParams>),
     #[serde(rename = "controlChild")]
